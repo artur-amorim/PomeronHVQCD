f676e2c3fea0d4dc2c31707ec08bfa810f49e40c
Artur Amorim <arturj.c.amorimdesousa@gmail.com>
2020-12-16 07:43:37 +0800

    Computed corrected Regge trajectories and sigmas.

---
+++ computeReggeTrajectories.sh
@@ -0,0 +1,5 @@
+./execs/computeReggeTrajectories.exe 0 -13.3221 0 0 0 0 0 -8.84064 0 0 0 0 0 1000 regge_trajectories_KernelFixed_As
+./execs/computeReggeTrajectories.exe 0 0 10.6221 0 0 0 0 0 6.53933 0 0 0 0 1000 regge_trajectories_KernelFixed_Bs
+./execs/computeReggeTrajectories.exe 0 0 0 1.0692 0 0 0 0 0 1.48022 0 0 0 1000 regge_trajectories_KernelFixed_Cs
+./execs/computeReggeTrajectories.exe 0 0 0 0 -9.86748 0 0 0 0 0 -5.38592 0 0 1000 regge_trajectories_KernelFixed_Ds
+./execs/computeReggeTrajectories.exe 0 0 0 0 0 0.246201 0 0 0 0 0 1.41797 0 1000 regge_trajectories_KernelFixed_Es
---
+++ fitCrossSectionsKernelFixed.sh
@@ -0,0 +1,5 @@
+./execs/fitAllSigmaFixedKernel.exe -13.3221 -8.84064 1 0.00163542 0.0153645 0.00190979 6.71494 -81.4951 24.6697 > fitAllSigmaKernelFixedAs.txt
+./execs/fitAllSigmaFixedKernel.exe 10.6221 6.53933 2 -0.000241862 -0.105668 0.0137332 -0.368672 -24.0912 33.3034 > fitAllSigmaKernelFixedBs.txt
+./execs/fitAllSigmaFixedKernel.exe 1.0692 1.48022 3 0.00174508 -0.0168147 0.0267856 1.1206 -14.309 12.9914 > fitAllSigmaKernelFixedCs.txt
+./execs/fitAllSigmaFixedKernel.exe -9.86748 -5.38592 4 0.000503324 0.0125652 0.01874 1.48272 -34.1906 61.9146 > fitAllSigmaKernelFixedDs.txt
+./execs/fitAllSigmaFixedKernel.exe 0.246201 1.41797 5 0.0307329 -0.126877 -0.0356684 1.23283 -16.2746 -13.9132 > fitAllSigmaKernelFixedEs.txt
