985caa501b74e0b016b75785e54e951613d66f66
Artur Amorim <arturj.c.amorimdesousa@gmail.com>
2020-12-23 13:24:44 +0800

    Added back the derivative terms to the potentials. Updated notes.

--- src/MesonKernel.cpp
+++ src/MesonKernel.cpp
@@ -22,7 +22,7 @@ std::vector<double> MesonKernel::computePotential(const double J) const
     // model = IHQCD
     std::vector<double> VSch = hvqcd().U1() + (J*J-1 - (J - 1) * invls * invls ) * hvqcd().e2A();
     VSch = VSch + (J-1) * ( (1.0 + invls * invls) * pow(hvqcd().dAstring(), 2) - hvqcd().d2Astring() / pow(hvqcd().G(), 2) );
-    //VSch = VSch + (J-1) * (a * hvqcd().aF() + b * hvqcd().bF() + c * hvqcd().cF() + d * hvqcd().dF() + e * hvqcd().eF());
+    VSch = VSch + (J-1) * (a * hvqcd().aF() + b * hvqcd().bF() + c * hvqcd().cF() + d * hvqcd().dF() + e * hvqcd().eF());
     // VSch's values are ordered from IR to UV
     // We want from UV to IR, so we reverse them
     std::reverse(std::begin(VSch), std::end(VSch));
